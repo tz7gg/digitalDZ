838099b75cd1b56a1027c8a5b767b8ee6ac3700a
tz7gg <69346690+tz7gg@users.noreply.github.com>
2022-12-20 15:52:30 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    //resize text area
+    //resize text area TODO
     const formTextArea = document.querySelector('#message')
     formTextArea.oninput = function() {
         this.style.height = `${this.scrollHeight}px`
